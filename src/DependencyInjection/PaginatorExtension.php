2c1b7938be9331673d35abef60fd27d362557c2a
Jack <J.A.C.K.009@hotmail.com>
2025-12-27 10:51:29 +0100

    use prepend

--- src/DependencyInjection/PaginatorExtension.php
+++ src/DependencyInjection/PaginatorExtension.php
@@ -5,26 +5,27 @@ namespace Jack009\PaginatorBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class PaginatorExtension extends Extension
+class PaginatorExtension extends Extension implements PrependExtensionInterface
 {
 
     public function load(array $configs, ContainerBuilder $container): void
     {
-        if ($container->hasDefinition('twig.loader.filesystem')) {
-            $container
-                ->getDefinition('twig.loader.filesystem')
-                ->addMethodCall(
-                    'addPath',
-                    [__DIR__ . '/../../templates', 'PaginatorBundle']
-                );
-        }
-
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../../config')
         );
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('twig', [
+            'paths' => [
+                __DIR__ . '/../../templates' => 'PaginatorBundle',
+            ],
+        ]);
     }
 }
