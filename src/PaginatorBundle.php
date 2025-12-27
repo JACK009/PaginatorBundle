fe31fe6d6b2a5ad546a34e6ec2739dedf94b4cb5
Jack <J.A.C.K.009@hotmail.com>
2025-12-27 11:59:08 +0100

    set containerextension

--- src/PaginatorBundle.php
+++ src/PaginatorBundle.php
@@ -2,9 +2,18 @@
 
 namespace Jack009\PaginatorBundle;
 
+use Jack009\PaginatorBundle\DependencyInjection\PaginatorExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PaginatorBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new PaginatorExtension();
+        }
 
+        return $this->extension;
+    }
 }
